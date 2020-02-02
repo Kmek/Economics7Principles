f98c7c168ffed173de76922b42d3cb772fc25aa7
Kmek <kmekeefe@gmail.com>
2020-02-02 18:21:56 -0500

    Make slides slide smoothly

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 /******************** HTML Components ********************/
 const mainSlide = document.getElementById("mainSlide")
 const slide2 = document.getElementById("slide2")
+const slide3 = document.getElementById("slide3")
 // const title = document.getElementById("slideTitle")
 // const description = document.getElementById("description")
 // const example1 = document.getElementById("example1")
@@ -17,12 +18,12 @@ const forward = document.getElementById("forward")
 function changeAccent(color) {
     document.documentElement.style.setProperty('--accent', color)
 }
-changeAccent("#cf190c") // Loads in nicely
+changeAccent("#2ea9ab") // Loads in nicely
 
 
 /******************** Fill the Slide ********************/
 var currentSlide = 0
-var onMainSlide = true
+// var onMainSlide = true
 
 function loadSlide(slide, number) {
     // Load info into the given slide
@@ -31,18 +32,79 @@ function loadSlide(slide, number) {
 /******************** Move the Slides ********************/
 // document.body.clientWidth // get the width of the screen
 
+var position = ["-700px", "calc(50% - 325px)", "100%"]
+var allSlides = [slide2, mainSlide, slide3]
+var current = 1;
+
+for (let s = 0; s < allSlides.length; s++) {
+    allSlides[s].style.left = position[s]
+}
+
+function changeSlide(direction) {
+    // current += direction
+    // if (current > 2)
+    //     current = 0
+    // else if (current < 0)
+    //     current = 2
+
+    // let hidden = 0
+    if (direction == -1) {
+        for (let s = 0; s < allSlides.length; s++) {
+            if (allSlides[s].style.left == position[0]) {
+                allSlides[s].style.left = position[1]
+                current = s
+                allSlides[s].classList.remove("hiddenSlide")
+            } else if (allSlides[s].style.left == position[1]) {
+                allSlides[s].style.left = position[2]
+                // hidden = s
+                allSlides[s].classList.add("hiddenSlide")
+            } else if (allSlides[s].style.left == position[2]) {
+                allSlides[s].style.left = position[0]
+            }
+        }
+    } else {
+        for (let s = 0; s < allSlides.length; s++) {
+            if (allSlides[s].style.left == position[0]) {
+                allSlides[s].style.left = position[2]
+            } else if (allSlides[s].style.left == position[1]) {
+                allSlides[s].style.left = position[0]
+                // hidden = s
+                allSlides[s].classList.add("hiddenSlide")
+            } else if (allSlides[s].style.left == position[2]) {
+                allSlides[s].style.left = position[1]
+                current = s
+                allSlides[s].classList.remove("hiddenSlide")
+            }
+        }
+    }
+
+    console.log(allSlides[current].id)    
+}
+changeSlide(1)
+changeSlide(1)
+changeSlide(1)
+
 back.onclick = function() {
-    console.log("switching back")
+    // console.log("switching back")
+    
+    changeSlide(-1)
+
+    
+    loadSlide(allSlides[current], slides[0])
+
+
     // Disable arrow buttons
-    // Move slides into position
+    // (done) Move slides into position
     // Fill slides with correct info
-    // Turn on transition-duration 2.0s
-    // Move slides to new position
-    // Switch classes for centerSlide
-    // Update currentSlide #
+    // (done) Turn on transition-duration 2.0s
+    // (done) Move slides to new position
+    // (done) Switch classes for centerSlide
+    // (done) Update currentSlide #
     // Enable arrow buttons
 }
 
 forward.onclick = function() {
-    console.log("switching forward")
+    // console.log("switching forward")
+
+    changeSlide(1)
 }

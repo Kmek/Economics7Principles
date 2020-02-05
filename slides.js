7323e2f37730d5e466ea496b632e14fd495f6992
Kmek <32857933+Kmek@users.noreply.github.com>
2020-02-05 11:02:48 -0500

    Add first two slides and all pictures to slides

--- slides.js
+++ slides.js
@@ -4,50 +4,50 @@ var slideIndex = 0
 
 var slides = [{
     title: "Scarcity Forces Tradeoffs",
-    description: "asdfg my desc, test <mark>mark</mark>",
-    image: "media/graph.png",
-    example1: "ex 1", 
-    example2: "ex 2",
+    description: "Through unlimited wants and limited resources, <mark>scarcity</mark> forces people to choose what they have, forcing <mark>trade-offs</mark> they sometimes aren’t even aware of. These trade-offs aren’t limited to money, as they also include time and opportunities lost to the chosen want/service/opportunity. The second choice option that was lost in a trade off is known as the <mark>opportunity cost</mark>.",
+    image: "media/Scarcity Forces Tradeoffs.png",
+    example1: "I don’t have infinite time, so to keep my grades afloat I have to spend an hour doing my homework instead of sleeping or binge watching Netflix, as I wish to.", 
+    example2: "The government doesn’t have infinite funding, so we can’t have free food or healthcare.",
     color: "#009bba"
 }, {
     title: "Costs vs. Benefits",
-    description: "asdfg my desc",
-    image: "media/graph.png",
-    example1: "ex 1", 
-    example2: "ex 2",
+    description: "A <mark>cost-benefit analysis</mark> can be used to compare, by weighing the <mark>costs</mark> vs the <mark>benefits</mark> of a choice, the outcomes of two wants to decide which to trade-off as an opportunity cost. This can be very subjective, as the <mark>benefits</mark> and <mark>costs</mark> are the money, time, effort, experience, or improvement that mainly relate to the person comparing their options.",
+    image: "media/Cost vs. Benefits.png",
+    example1: "I could watch Netflix for an hour because it would make me happy, or I could study for an upcoming test so I am well prepared.", 
+    example2: "College tuition could be cheaper, but expenses like new stadiums are more important to the college’s status, so tuition prices are raised to the college’s interests.",
     color: "#3da13d"
 }, {
     title: "People Think at the Margin",
     description: "asdfg my desc",
-    image: "media/graph.png",
+    image: "media/People think at the margin.png",
     example1: "ex 1", 
     example2: "ex 2",
     color: "#d9840f"
 }, {
     title: "Incentives Matter",
     description: "asdfg my desc",
-    image: "media/graph.png",
+    image: "https://i.redd.it/9nr2w3mhbfa11.png",
     example1: "ex 1", 
     example2: "ex 2",
     color: "#9e6bbf"
 }, {
     title: "Trade Makes People Better Off",
     description: "asdfg my desc",
-    image: "media/graph.png",
+    image: "https://www.dea2020.com/files/cache/4e700e64ab66ed01cb45b915a5cff60d.jpg",
     example1: "ex 1", 
     example2: "ex 2",
     color: "#a62b56"
 }, {
     title: "Markets Coordinate Trade",
     description: "asdfg my desc",
-    image: "media/graph.png",
+    image: "https://media.olivegarden.com/en_us/images/marketing/italian-family-restaurant-olive-garden-g6-rdv.jpg",
     example1: "ex 1", 
     example2: "ex 2",
     color: "#363391"
 }, {
     title: "Future Consequences Count",
     description: "asdfg my desc",
-    image: "media/graph.png",
+    image: "media/Future Consequences Count.png",
     example1: "ex 1", 
     example2: "ex 2",
     color: "#21b08f"
